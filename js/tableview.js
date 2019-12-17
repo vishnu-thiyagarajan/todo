6c97414a9063910ff06318dced20f330a6c64687
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2019-12-17 17:07:36 +0530

    rename and search

--- js/tableview.js
+++ js/tableview.js
@@ -1,5 +1,7 @@
 "use strict";
 let toDoObj = []
+let checkedCount = 0
+let lastCheckEvent;
 function addNewList(){
   var addNewList = document.getElementById("myForm")
   var newListBtn = document.getElementById("newlist")
@@ -28,9 +30,11 @@ function enableCrt(){
   if (listName.value=="") return disable(crtListBtn)
   enable(crtListBtn)
 }
+
 function closeForm(){
   document.getElementById("myForm").style.display = "none";
-  document.getElementById("listname").setAttribute("autofocus","autofocus");
+  document.getElementById("renameForm").style.display = "none";
+  // window.removeEventListener('click', closePopup(event));
 }
 
 function createList(){
@@ -46,6 +50,7 @@ function tableCreate() {
   var countOfList = 0
   var noOfElements = toDoObj.length;
   var tbl = document.getElementById("tbl");
+  closeForm();
   if (tbl) tbl.remove();
   var body = document.getElementsByTagName("body")[0];
   var elemInRow = parseInt(window.innerWidth / 200) - 1;
@@ -89,6 +94,7 @@ function tableCreate() {
 
 function selectList(){
   var buttonList = document.getElementsByClassName("listButton");
+  checkedCount = 0
   for (var i = 0; i < buttonList.length; i++) {
     if (buttonList[i].childNodes.length == 2) {
       document.getElementsByTagName("footer")[0].style.display = "none";
@@ -96,20 +102,88 @@ function selectList(){
       continue
     }
     document.getElementsByTagName("footer")[0].style.display = "flex";
+    document.getElementById("delete").className = "ctadisable";
+    document.getElementById("rename").className = "ctadisable"
     var cord = buttonList[i].getBoundingClientRect();
     var checkbox = document.createElement("INPUT");
     checkbox.setAttribute("type", "checkbox");
     checkbox.setAttribute("class", "chkbox");
+    checkbox.setAttribute("onchange", "countCheck(event)");
     checkbox.style.top = (cord.bottom/4) - 10+"px";
     buttonList[i].appendChild(checkbox);
   }
 }
 
 function deleteSelected(){
+  if (document.getElementById("delete").className == "ctadisable") return
   var checkList = document.getElementsByClassName("chkbox");
   toDoObj = toDoObj.filter(function(value, index, arr){
     return !checkList[index].checked;
   });
   selectList()
   tableCreate()
+}
+
+function countCheck(event){
+  if (event.target.checked) {lastCheckEvent = event; checkedCount += 1}
+  if (!event.target.checked) checkedCount -= 1
+  if (checkedCount == 1) document.getElementById("rename").setAttribute("class","cta")
+  if (checkedCount != 1) document.getElementById("rename").setAttribute("class","ctadisable")
+  if (checkedCount == 0) document.getElementById("delete").setAttribute("class","ctadisable")
+  if (checkedCount > 0) document.getElementById("delete").setAttribute("class","cta")
+}
+
+function renameSelected(){
+  if (document.getElementById("rename").className == "ctadisable") return
+  var renameForm = document.getElementById("renameForm")
+  var elemToBeRenamed = lastCheckEvent.target.parentNode.parentNode.getElementsByTagName("p")[0]
+  var renamePopupPos = elemToBeRenamed.getBoundingClientRect();
+  renameForm.style.display = "block";
+  renameForm.style.opacity = "1";
+  renameForm.style.top = (renamePopupPos.bottom + renamePopupPos.height)+"px";
+  renameForm.style.left = (renamePopupPos.left + (renamePopupPos.width/5))+"px";
+  var newName = document.getElementById("newName")
+  newName.value = elemToBeRenamed.innerText
+  newName.focus();
+}
+
+function rename(){
+  var checkList = document.getElementsByClassName("chkbox");
+  for (var i=0;i<checkList.length;i++){
+    if (checkList[i].checked) break
+  }
+  var newName = document.getElementById("newName").value
+  var oldName = Object.keys(toDoObj[i])[0]
+  toDoObj[i][newName] = Object.values(toDoObj[i])[0]
+  delete toDoObj[i][oldName];
+  selectList();
+  tableCreate();
+}
+
+window.addEventListener('click', function(e){
+  var renameForm = document.getElementById('renameForm')
+	if (!(renameForm.style.display != "none" && renameForm.contains(e.target))){
+    renameForm.style.display = "none"
+  }
+},true)
+
+function highlight(event){
+  console.log(event.target.parentNode.parentNode.childNodes)
+  let nodes = event.target.parentNode.parentNode.childNodes;
+  nodes.forEach(element => {
+    if (element.hasChildNodes()) element.firstChild.style.color= "white"
+  });
+  event.target.style.color="#0088a9";
+}
+
+function attachSearch(){
+  const searchBar = document.getElementById("searchBar")
+  searchBar.style.display = searchBar.style.display == "flex" ? "none" : "flex"
+  if (searchBar.style.display == "none") tableCreate()
+  if (searchBar.style.display == "flex") document.getElementById("srchBar").focus();
+}
+
+function searchList(event){
+  if (event.keyCode !== 13) return
+  
 }
